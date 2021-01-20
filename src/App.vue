b40864b311dd3c33ed021fcc53507526bf25976b
Adam <adam@primestar.plus>
2021-01-20 09:38:40 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Baccarat msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Baccarat from './components/Baccarat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Baccarat
   }
 }
 </script>
@@ -23,6 +22,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+body {
+  margin: 0;
 }
 </style>
