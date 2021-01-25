54c6c1a2a4409423e37dbfbae31185b93b11223b
penbillpopo <penbillpopo@gmail.com>
2021-01-25 17:13:56 +0800

    新增home component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,15 @@
 <template>
   <div id="app">
-    <!-- <Baccarat msg="Welcome to Your Vue.js App"/> -->
-    <div class="container">
-      <div class="left">
-        <div class="top">
-          <OpenCard />
-          <Information />
-        </div>
-        <div class="bottom">
-          <History />
-        </div>
-      </div>
-      <div class="right">
-        <Blockchain />
-      </div>
-    </div>
+    <Home />
   </div>
 </template>
 
 <script>
-// import Baccarat from './components/Baccarat.vue'
-import History from './views/history/index.vue'
-import Blockchain from './views/blockchain/index.vue'
-import Information from './views/information/index.vue'
-import OpenCard from './views/open-card/index.vue'
-
+import Home from './views/home.vue'
 export default {
   name: 'App',
   components: {
-    // Baccarat,
-    History,
-    Blockchain,
-    Information,
-    OpenCard
-  },
-  data() {
-    return {
-    }
-  },
-  computed: {
-  },
-  mounted() {
-    this.$store.dispatch('app/getInit')
+    Home
   }
 }
 </script>
@@ -54,29 +22,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   /* margin-top: 60px; */
-}
-body {
-  margin: 0;
-}
-.container {
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  .left {
-    display: flex;
-    flex-direction: column;
-    width: 90%;
-    border-right: 1px solid #000;
-    .top {
-      display: flex;
-      height: 100%;
-      border-bottom: 1px solid #000;
-    }
-    .bottom {
-    }
-  }
-  .right {
-    flex: 1;
-  }
 }
 </style>
---
+++ src/views/home.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <!-- <Baccarat msg="Welcome to Your Vue.js App"/> -->
+
+    <div class="container">
+      <div class="left">
+        <div class="top">
+          <OpenCard />
+          <Information />
+        </div>
+        <div class="bottom">
+          <History />
+        </div>
+      </div>
+      <div class="right">
+        <Blockchain />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// import Baccarat from './components/Baccarat.vue'
+import History from './history/index.vue'
+import Blockchain from './blockchain/index.vue'
+import Information from './information/index.vue'
+import OpenCard from './open-card/index.vue'
+export default {
+  name: 'Home',
+  components: {
+    History,
+    Blockchain,
+    Information,
+    OpenCard
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    this.$store.dispatch('app/getInit')
+  }
+}
+</script>
+<style lang="scss" scoped>
+body {
+  margin: 0;
+}
+.container {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  .left {
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    border-right: 1px solid #000;
+    .top {
+      display: flex;
+      height: 100%;
+      border-bottom: 1px solid #000;
+    }
+    .bottom {
+    }
+  }
+  .right {
+    flex: 1;
+  }
+}
+</style>
